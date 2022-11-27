14d1010d20daed265c4b5f80550ad73bc140eec2
riccardiunix <riccardo.bordon.2014@gmail.com>
2022-11-27 18:57:18 +0100

    update geckodriver

--- install-geckodriver.sh
+++ install-geckodriver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Versione modificata di https://askubuntu.com/questions/870530/how-to-install-geckodriver-in-ubuntu
 INSTALL_DIR="/usr/local/bin"
-wget -q --show-progress "https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz" -O geckodriver.tar.gz
+wget -q --show-progress "https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz" -O geckodriver.tar.gz
 tar -xzf geckodriver.tar.gz && rm geckodriver.tar.gz
 chmod +x geckodriver
 sudo mv geckodriver "$INSTALL_DIR"
